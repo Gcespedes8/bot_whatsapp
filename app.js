9ff316b47902ff1160fe23c9aa1c6e8a46fa4e80
Gonzalo Cespedes <g.cespedes8199@icloud.com>
2022-09-30 20:58:28 -0500

    QR session created

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
+const fs = require('fs');
 const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const qrcode = require('qrcode-terminal');
 
-client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
-});
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
+const SESSION_FILE_PATH = '/session.json';
+let client;
+let sessionData;
 
-client.initialize();
+const withSession = () => {
+
+};
+
+
+// This function generates the QRCode
+const withOutSession = () => {
+    console.log('No session started');
+    client = new Client();
+    client.on('qr', qr =>{
+        qrcode.generate(qr, {small: true});
+    });
+
+    client.on('authenticated', (session) => {
+        // Safe the credentials for further use
+        sessionData = session;
+        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
+            if (err) {
+                console.log(err);
+            }
+        })
+    })
+};
+
+//Condición tenaria: condición en una sola línea
+(fs.existsSync(SESSION_FILE_PATH)) ? withSession() : withOutSession();
+
